cddad616ccf6090fe62af4dce3e75dc509061ef7
Julia <juliarayannef.moraes@gmail.com>
2025-03-18 20:53:21 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+let amigos = [];
+
+//Função para adicionar um amigo
+function adicionarAmigo() {
+    const inputAmigo = document.getElementById('amigo');
+    const nomeAmigo = inputAmigo.value.trim();
+    inputAmigo.Value = "";
+
+//Ter a certeza que o campo não ficou vazio
+if (nomeAmigo == "") {
+    alert('Por favor, insira um nome.');
+    return;
+}
+
+//nome não repetido
+if(amigos.includes(nomeAmigo)) {
+    alert(`O nome ${nomeAmigo} já está adicionado na lista`);
+    return;
+}
+  
+//Adicionar o nome ao array de amigos
+amigos.push(nomeAmigo);
+
+
+//Limpar o campo de entrada
+inputAmigo.value = "";
+
+//Atualizar a lista
+atualizarLista();
+}
+
+function atualizarLista() {
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ""; // Limpar o campo
+    for(let i = 0; i <amigos.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li) // Criar sequência na lista
+
+    }
+
+}
+
+function sortearAmigo() {
+    if(amigos.length == 0) {
+        alert('Não tem como sortear. É preciso ter pelo menos um.');
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    const amigoSorteado = amigos[indiceAleatorio]; 
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `Amigo sorteado: <strong>${amigoSorteado}</strong`; // strong usado para dar importância ao texto
+
+}
